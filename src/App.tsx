597503abdb9c41169fa4a123d234cb62e1c13bb2
yushan <yushan@intervest.lk>
2021-10-16 19:48:42 +0530

    feat: useState - Organizing Interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,29 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { User } from "./interfaces";
 
-import './App.css';
-
-interface Address {
-  street: 'Main st.',
-  number: 88,
-  zip: '21345'
-}
-
-interface User {
-  name: 'Mitchel',
-    age: 23,
-    country: 'the Netherlands',
-    address: Address,
-    admin: false
-}
+import "./App.css";
 
 function App() {
   const [user, setUser] = useState<User | null>(null);
 
-  const fetchUser = () => setUser({
-    name: 'Mitchel',
-    age: 23,
-    country: 'the Netherlands',
-    address: {
-      street: 'Main st.',
-      number: 88,
-      zip: '21345'
-    },
-    admin: false
-  });
+  const fetchUser = () =>
+    setUser({
+      name: "Mitchel",
+      age: 23,
+      country: "the Netherlands",
+      address: {
+        street: "Main st.",
+        number: 88,
+        zip: "21345",
+      },
+      admin: false,
+    });
 
   return (
     <div className="App">
       <header className="App-header">
         <button onClick={fetchUser}>Fetch user on click</button>
-        {user && <p>{`Welcome to ${user.name}`}</p>}        
+        {user && <p>{`Welcome to ${user.name}`}</p>}
       </header>
     </div>
   );

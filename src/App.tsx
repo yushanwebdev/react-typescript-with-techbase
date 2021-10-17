562fdd93d55fe324f84359b186f30d00af89c9a4
yushan <yushan@intervest.lk>
2021-10-17 14:07:05 +0530

    feat: Generic Components - Develop <DataGrid /> as a generic component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import DataGrid from "./components/DataGrid";
 
+export interface User {
+  id: number;
+  name: string;
+  age: number;
+}
+
 export default function App() {
-  const users = [
+  const users: User[] = [
     { id: 1, name: "John", age: 55 },
     { id: 2, name: "Mitchel", age: 23 },
     { id: 3, name: "Mike", age: 50 },

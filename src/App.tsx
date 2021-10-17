1fb8a6507de72f93260a3033d9c3af2b0d652cac
yushan <yushan@intervest.lk>
2021-10-17 08:26:08 +0530

    feat: Setup the application for the next Chapter

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,15 @@
-import { useState } from "react";
+import { useContext } from "react";
+import { InputValueContext } from "./context/InputValueContext";
 
-import "./App.css";
-
-const defaultFormData = {
-  title: "",
-  body: "",
-};
-
-function App() {
-  const [formData, setFormData] = useState(defaultFormData);
-  const { title, body } = formData;
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.id]: e.target.value,
-    }));
-  };
-
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(formData);
-
-    setFormData(defaultFormData);
-  };
+export default function App() {
+  const { state, dispatch } = useContext(InputValueContext);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Form</h1>
-        <p>Create a post</p>
-
-        <form onSubmit={onSubmit}>
-          <label htmlFor="title">Title</label>
-          <br />
-          <input type="text" id="title" value={title} onChange={onChange} />
-          <br />
-          <br />
-          <label htmlFor="body">Body</label>
-          <br />
-          <input type="text" id="body" value={body} onChange={onChange} />
-          <br />
-          <br />
-          <button type="submit">Upload post</button>
-        </form>
-      </header>
-    </div>
+    <>
+      <p>Value: {state.inputValue}</p>
+      <button onClick={() => dispatch({ type: "SET_INPUT_VALUE_TO_100" })}>
+        SET VALUE TO 100
+      </button>
+    </>
   );
 }
-
-export default App;
